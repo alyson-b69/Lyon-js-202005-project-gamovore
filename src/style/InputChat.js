b1ddcd6077472d62e83e6435096a53b6d1a73ef8
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-11 11:52:51 +0200

    add function to send message on press enter

---
+++ src/style/InputChat.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const InputChat = styled.input`
+  background-color: #4e4643;
+  color: #ffffff;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  width: 98%;
+  cursor: default;
+`;
+
+export default InputChat;
